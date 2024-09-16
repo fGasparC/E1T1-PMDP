1adcdb478de0a360cd2efab8c0cf45edd41b520e
fGasparC <franciscogasparcasadogarcia@gmail.com>
2024-09-16 17:54:12 +0200

    E1T1 Se añaden botones y listeners

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,18 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val boton1 = findViewById<Button>(R.id.button)
+        val boton2 = findViewById<Button>(R.id.button2)
+        val texto = findViewById<TextView>(R.id.textView2)
+
+        boton1.setOnClickListener{
+            texto.text ="Hola mundo!"
+        }
+        boton2.setOnClickListener{
+            texto.text="Hello world!"
+
         }
     }
+
 }
